90a0a00b3737b6c88a4922802a2692ca423d6687
Antony Evmorfopoulos <antonhs.evm@outlook.com>
2025-06-17 06:32:55 +0100

    feat: add picture

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,28 @@
 <template>
-    <h1>About Chemical Marine Inspections</h1>
-    <p>Chemical Marine Inspections was established in 2004 in Piraeus, Greece, a major centre for the marine industry.
-        Our company looks forward to becoming:</p>
-    <ul>
-        <li>a reliable supplier for gas detection instruments, spare parts and their consumables.</li>
-        <li>a reliable service station for servicing gas detection instruments.</li>
-        <li>a reliable source of information related to safety issues in the marine industry.</li>
-    </ul>
-    Our extensive knowledge and expertise in gas detection along with the cooperation with internationally recognized
-    manufacturers allows Chemical Marine Inspections to stand out of the competition and become the leading Greek
-    company dealing with gas detection safety issues for the marine industry.
+    <div class="h-screen w-screen overflow-x-hidden">
+        <section class="snap-start h-screen flex items-center justify-center bg-cover bg-center text-white relative"
+            :style="{ backgroundImage: `url('/src/assets/frontpage/frontpage_3.jpg')` }">
+            <div class="bg-black bg-opacity-50 w-full h-full absolute top-0 left-0"></div>
+            <div class="z-10 text-center">
+                <h2 class="text-4xl md:text-6xl font-bold">Chemical Marine Inspections</h2>
+                <p class="text-xl mt-4">Gas Detectors — Span Gases — Spare Parts</p>
+            </div>
+        </section>
+
+
+        <h1>About Chemical Marine Inspections</h1>
+        <p>Chemical Marine Inspections was established in 2004 in Piraeus, Greece, a major centre for the marine
+            industry. Our company looks forward to becoming:</p>
+        <ul>
+            <li>a reliable supplier for gas detection instruments, spare parts and their consumables.</li>
+            <li>a reliable service station for servicing gas detection instruments.</li>
+            <li>a reliable source of information related to safety issues in the marine industry.</li>
+        </ul>
+        <p>Our extensive knowledge and expertise in gas detection along with the cooperation with internationally
+            recognized manufacturers allows Chemical Marine Inspections to stand out of the competition and become the
+            leading Greek
+            company dealing with gas detection safety issues for the marine industry.
+        </p>
+    </div>
+
 </template>
