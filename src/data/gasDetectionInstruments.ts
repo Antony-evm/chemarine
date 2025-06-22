91e45aa21d654d74aac6ee544a1c10eb08b06721
Antony Evmorfopoulos <antonhs.evm@outlook.com>
2025-06-22 19:38:29 +0100

    more riken

--- src/data/gasDetectionInstruments.ts
+++ src/data/gasDetectionInstruments.ts
@@ -2,6 +2,8 @@ import { gmi_ps_200 } from "./instruments/gmi_ps_200";
 import { gmi_ps_500 } from "./instruments/gmi_ps_500";
 import { rki_gx_3r } from "./instruments/rki_gx_3r";
 import { rki_gx_3r_pro } from "./instruments/rki_gx_3r_pro";
+import { rki_gx_6000 } from "./instruments/rki_gx_6000";
+import { rki_gx_force } from "./instruments/rki_gx_force";
 
 export const GAS_DETECTION_INSTRUMENTS = [
   {
@@ -14,6 +16,8 @@ export const GAS_DETECTION_INSTRUMENTS = [
       riken: {
         rki_gx_3r,
         rki_gx_3r_pro,
+        rki_gx_force,
+        rki_gx_6000,
       },
     },
   },
@@ -23,11 +27,10 @@ export const GAS_DETECTION_INSTRUMENTS = [
       gmi: {
         gmi_ps_500,
       },
+      riken: {
+        rki_gx_6000,
+      },
     },
-  },
-  {
-    title: "Fixed",
-    instruments: {},
   },
   {
     title: "Calibration Stations",
