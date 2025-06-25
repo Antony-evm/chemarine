c2a9fe8ad5e7b8a65e852253fdf15aeed2270456
Antony Evmorfopoulos <antonhs.evm@outlook.com>
2025-06-25 18:01:58 +0100

    fix instrument loading

--- src/pages/GasDetectionInstrumentDetailPage.vue
+++ src/pages/GasDetectionInstrumentDetailPage.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import PageSection from "@/components/utils/PageSection.vue";
+import ContentSection from "@/components/utils/ContentSection.vue";
 import SpaceComponent from "@/components/utils/SpaceComponent.vue";
+import SectionDivider from "@/components/utils/SectionDivider.vue";
+import CenteredContent from "@/components/utils/CenteredContent.vue";
 import { useRoute } from "vue-router";
 import { GAS_DETECTION_INSTRUMENTS } from "@/data/gasDetectionInstruments";
 import { ref, onMounted } from "vue";
@@ -14,8 +16,7 @@ const instrument = ref<InstrumentData | null>(null);
 onMounted(() => {
     for (const section of GAS_DETECTION_INSTRUMENTS) {
         if (section.instruments[category as string] && section.instruments[category as string][instrumentKey as string]) {
-            instrument.value = section.instruments[category as string][instrumentKey as string];
-            break;
+            instrument.value = section.instruments[category as string][instrumentKey as string]; break;
         }
     }
 });
@@ -23,35 +24,23 @@ onMounted(() => {
 
 <template>
     <div v-if="instrument">
-        <PageSection :sections="[
-            {
-                title: instrument.title,
-                titleTag: 'h1',
-                content: 'features'
-            },
-            {
-                title: 'Overview',
-                titleTag: 'h2',
-                content: 'overview'
-            }
-        ]" with-page-spacing>
-            <template #features>
-                <div class="flex justify-between">
-                    <img :src="instrument.imgPath" :alt="instrument.alt" loading="lazy"
-                        class="w-70 h-50 object-contain" />
-                    <ul class="mt-4 md:ml-8 list-disc list-inside">
-                        <li v-for="f in instrument.features" :key="f" v-html="f" class="mb-1" />
-                    </ul>
-                </div>
-                <SpaceComponent size="paragraph" />
-            </template>
+        <ContentSection :title="instrument.title" title-tag="h1" with-page-spacing>
+            <div class="flex justify-between">
+                <img :src="instrument.imgPath" :alt="instrument.alt" loading="lazy" class="w-70 h-50 object-contain" />
+                <ul class="mt-4 md:ml-8 list-disc list-inside">
+                    <li v-for="f in instrument.features" :key="f" v-html="f" class="mb-1" />
+                </ul>
+            </div>
+            <SpaceComponent size="paragraph" />
+        </ContentSection>
 
-            <template #overview>
-                <p v-for="o in instrument.overview" :key="o">{{ o }}
-                    <SpaceComponent size="paragraph" />
-                </p>
-            </template>
-        </PageSection>
+        <CenteredContent>
+            <h2>Overview</h2>
+            <SectionDivider />
+            <p v-for="o in instrument.overview" :key="o">{{ o }}
+                <SpaceComponent size="paragraph" />
+            </p>
+        </CenteredContent>
     </div>
     <div v-else>
         <p>Instrument not found.</p>
