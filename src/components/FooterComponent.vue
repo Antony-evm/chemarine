ba92edf36d567c2c1f0267903be5206c395d08ba
Antony Evmorfopoulos <antonhs.evm@outlook.com>
2025-06-26 06:42:17 +0100

    responsive footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -3,18 +3,21 @@ import { PRIMARY_EMAIL, PHONE_NO, COMPANY_NAME } from '@/data/contactInformation
 </script>
 <template>
     <footer class="bg-[#0a1f3d] padding w-full">
-        <div class="flex flex-row items-center justify-between">
-            <div class="text-center">
+        <div class="flex flex-col items-center text-center
+             sm:flex-row sm:justify-between sm:items-start
+             gap-6">
+            <div>
                 <p class="text-lg font-bold">{{ COMPANY_NAME }}</p>
                 <p class="text-sm">Gas Detectors — Span Gases — Spare Parts</p>
                 <div class="text-center text-xs text-white/50">
                     © {{ new Date().getFullYear() }} {{ COMPANY_NAME }}. All rights reserved.
                 </div>
             </div>
-            <div class="text-center text-lg">
+            <div>
                 <p>Email: <a href="mailto:chemarin@otenet.gr" class="hover:underline">{{ PRIMARY_EMAIL }}</a>
                 </p>
-                <p>Phone: <a href="tel:+302104688245" class="hover:underline">{{ PHONE_NO }}</a></p>
+                <p class="mt-2">
+                    Phone: <a href="tel:+302104688245" class="hover:underline">{{ PHONE_NO }}</a></p>
             </div>
 
         </div>
